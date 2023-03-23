b7224edca1b987504c7614e0304a37380ebb24ae
akhilesh <akhileshinglege@gmail.com>
2023-03-23 13:45:27 +0530

    initial commit

---
+++ client/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	pb "grpc-crud/proto"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const address = "localhost:8080"
+
+type Movie struct {
+	Id    int32  `json:"id"`
+	Title string `json:"title"`
+	Genre string `json:"genre"`
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect")
+	}
+	defer conn.Close()
+
+	client := pb.NewMovieServiceClient(conn)
+
+	router := gin.Default()
+
+	router.GET("/movies", func(ctx *gin.Context) {
+		res, err := client.GetMovies(ctx, &pb.GetMoviesRequest{})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Movies not found",
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    res,
+			"message": "Fetched data successfully",
+		})
+	})
+
+	router.GET("/movie/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		res, err := client.GetMovie(ctx, &pb.GetMovieRequest{Id: id})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Movie not found",
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    res.Movie,
+			"message": "Record fetched successfully",
+		})
+	})
+
+	router.POST("/movie", func(ctx *gin.Context) {
+		var movie Movie
+
+		err := ctx.ShouldBindJSON(&movie)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Error in movie creation",
+			})
+			return
+		}
+		data := pb.Movie{
+			Title: movie.Title,
+			Genre: movie.Genre,
+		}
+		res, err := client.CreateMovie(ctx, &pb.CreateMovieRequest{
+			Movie: &data,
+		})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    res.Movie,
+			"message": "Movie created successfully",
+		})
+	})
+
+	router.PUT("/movie/:id", func(ctx *gin.Context) {
+		var movie Movie
+		id := ctx.Param("id")
+		idint, _ := strconv.Atoi(id)
+		idInt := int32(idint)
+		err := ctx.ShouldBindJSON(&movie)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Error in getting user data",
+			})
+			return
+		}
+		res, err := client.UpdateMovie(ctx, &pb.UpdateMovieRequest{
+			Movie: &pb.Movie{
+				Id:    idInt,
+				Title: movie.Title,
+				Genre: movie.Genre,
+			},
+		})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Movie not found",
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"data":    res.Movie,
+			"message": "Record updated successfully",
+		})
+	})
+
+	router.DELETE("/movie/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		res, err := client.DeleteMovie(ctx, &pb.DeleteMovieRequest{Id: id})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Movie not found",
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"Status": res.Success,
+		})
+	})
+
+	// Start server
+	router.Run(":8000")
+}
---
+++ server/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+
+	pb "grpc-crud/proto"
+
+	"google.golang.org/grpc"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Movie struct {
+	Id    int32  `json:"id" gorm:"primaryKey autoIncrement"`
+	Title string `json:"title"`
+	Genre string `json:"genre"`
+}
+
+const port = ":8080"
+
+var DB *gorm.DB
+var dsn = "postgres://postgres:12345@localhost:5432/grpc?sslmode=disable"
+
+func SetupDB() {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Printf("Failed to connect to database: %v\n", err)
+	}
+	db.AutoMigrate(&Movie{})
+	DB = db
+	log.Println("Connected to database..")
+}
+
+func init() {
+	SetupDB()
+}
+
+type movieServer struct {
+	pb.MovieServiceServer
+}
+
+// Get all movies
+func (s *movieServer) GetMovies(ctx context.Context, req *pb.GetMoviesRequest) (*pb.GetMoviesResponse, error) {
+	fmt.Println("Get Movies")
+	movies := []*pb.Movie{}
+	res := DB.Find(&movies)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Movie not found")
+	}
+	return &pb.GetMoviesResponse{
+		Movies: movies,
+	}, nil
+}
+
+// Get a movie record
+func (s *movieServer) GetMovie(ctx context.Context, req *pb.GetMovieRequest) (*pb.GetMovieResponse, error) {
+	fmt.Println("Get Movie")
+	var movie Movie
+	res := DB.First(&movie, req.Id)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Movie not found")
+	}
+	return &pb.GetMovieResponse{
+		Movie: &pb.Movie{
+			Title: movie.Title,
+			Genre: movie.Genre,
+		},
+	}, nil
+}
+
+// Create a movie record
+func (s *movieServer) CreateMovie(ctx context.Context, req *pb.CreateMovieRequest) (*pb.CreateMovieResponse, error) {
+	fmt.Println("Create Movie")
+	movie := req.Movie
+
+	data := Movie{
+		Title: movie.Title,
+		Genre: movie.Genre,
+	}
+	res := DB.Create(&data)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Error in movie creation")
+	}
+	return &pb.CreateMovieResponse{
+		Movie: &pb.Movie{
+			Title: movie.Title,
+			Genre: movie.Genre,
+		},
+	}, nil
+}
+
+// Update a movie record
+func (s *movieServer) UpdateMovie(ctx context.Context, req *pb.UpdateMovieRequest) (*pb.UpdateMovieResponse, error) {
+	fmt.Println("Update movie record")
+	var movie Movie
+	reqMovie := req.Movie
+	res := DB.Model(&movie).Where("id = ?", reqMovie.Id).Updates(Movie{
+		Title: reqMovie.Title,
+		Genre: reqMovie.Genre,
+	})
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Movie not found")
+	}
+	return &pb.UpdateMovieResponse{
+		Movie: &pb.Movie{
+			Title: movie.Title,
+			Genre: movie.Genre,
+		},
+	}, nil
+}
+
+// Delete a movie record
+func (s *movieServer) DeleteMovie(ctx context.Context, req *pb.DeleteMovieRequest) (*pb.DeleteMovieResponse, error) {
+	fmt.Println("Delete Movie record")
+	var movie Movie
+	res := DB.Delete(&movie, req.Id)
+	// res := DB.Where("id = ?", req.Id).Delete(&movie)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Movie not found")
+	}
+
+	return &pb.DeleteMovieResponse{
+		Success: true,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start server")
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterMovieServiceServer(grpcServer, &movieServer{})
+	log.Printf("Server listening at: %v", lis.Addr())
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+}
